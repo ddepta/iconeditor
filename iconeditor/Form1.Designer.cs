df3c8bbd26eae58a883eaa0651cccf16616a3357
Daniel <Daniel@DESKTOP-KJBOI1M>
2020-10-18 01:36:21 +0200

    temp changes

--- iconeditor/Form1.Designer.cs
+++ iconeditor/Form1.Designer.cs
@@ -28,21 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.canvas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Controls.Add(this.btnPlus);
+            this.canvas.Controls.Add(this.btnMinus);
+            this.canvas.Controls.Add(this.trackBar1);
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(554, 564);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(71, 478);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(23, 23);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.BtnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(42, 478);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(23, 23);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.BtnMinus_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 50;
+            this.trackBar1.Location = new System.Drawing.Point(12, 507);
+            this.trackBar1.Maximum = 150;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(530, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 564);
+            this.ClientSize = new System.Drawing.Size(554, 564);
+            this.Controls.Add(this.canvas);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.canvas.ResumeLayout(false);
+            this.canvas.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
     }
 }
 
--- iconeditor/Form1.cs
+++ iconeditor/Form1.cs
@@ -12,14 +12,124 @@ namespace iconeditor
 {
     public partial class Form1 : Form
     {
+        Graphics graphics;
+        int x = -1;
+        int y = -1;
+        bool moving = false;
+        Pen pen;
+        Brush brush = (Brush)Brushes.Black;
+        int zoom = 1;
+        bool initialized = false;
         public Form1()
         {
             InitializeComponent();
+
+            graphics = canvas.CreateGraphics();
+            pen = new Pen(Color.Black, 1);
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Square;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Square;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            graphics.ScaleTransform(zoom, zoom);
+            if(!initialized)
+            {
+                graphics.FillRectangle(brush, 1, 1, 1, 1);
+                graphics.FillRectangle(brush, 1, 3, 1, 1);
+                graphics.FillRectangle(brush, 1, 5, 1, 1);
+                graphics.FillRectangle(brush, 1, 7, 1, 1);
+                graphics.FillRectangle(brush, 1, 9, 1, 1);
+                graphics.FillRectangle(brush, 1, 11, 1, 1);
+
+                graphics.FillRectangle(brush, 2, 2, 1, 1);
+                graphics.FillRectangle(brush, 2, 4, 1, 1);
+                graphics.FillRectangle(brush, 2, 6, 1, 1);
+                graphics.FillRectangle(brush, 2, 8, 1, 1);
+                graphics.FillRectangle(brush, 2, 10, 1, 1);
+                graphics.FillRectangle(brush, 2, 12, 1, 1);
+
+                graphics.FillRectangle(brush, 3, 1, 1, 1);
+                graphics.FillRectangle(brush, 3, 3, 1, 1);
+                graphics.FillRectangle(brush, 3, 5, 1, 1);
+                graphics.FillRectangle(brush, 3, 7, 1, 1);
+                graphics.FillRectangle(brush, 3, 9, 1, 1);
+                graphics.FillRectangle(brush, 3, 11, 1, 1);
+
+                graphics.FillRectangle(brush, 4, 2, 1, 1);
+                graphics.FillRectangle(brush, 4, 4, 1, 1);
+                graphics.FillRectangle(brush, 4, 6, 1, 1);
+                graphics.FillRectangle(brush, 4, 8, 1, 1);
+                graphics.FillRectangle(brush, 4, 10, 1, 1);
+                graphics.FillRectangle(brush, 4, 12, 1, 1);
+
+                graphics.FillRectangle(brush, 5, 1, 1, 1);
+                graphics.FillRectangle(brush, 5, 3, 1, 1);
+                graphics.FillRectangle(brush, 5, 5, 1, 1);
+                graphics.FillRectangle(brush, 5, 7, 1, 1);
+                graphics.FillRectangle(brush, 5, 9, 1, 1);
+                graphics.FillRectangle(brush, 5, 11, 1, 1);
+
+                graphics.FillRectangle(brush, 6, 2, 1, 1);
+                graphics.FillRectangle(brush, 6, 4, 1, 1);
+                graphics.FillRectangle(brush, 6, 6, 1, 1);
+                graphics.FillRectangle(brush, 6, 8, 1, 1);
+                graphics.FillRectangle(brush, 6, 10, 1, 1);
+                graphics.FillRectangle(brush, 6, 12, 1, 1);
+
+                initialized = true;
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(moving && x != -1 && y != -1)
+            {
+                graphics.DrawLine(pen, new Point(x, y), e.Location);
+
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            //zoom = trackBar1.Value / 100f;
+            canvas.Invalidate();
+            Console.WriteLine(zoom.ToString());
+        }
+        private void BtnMinus_Click(object sender, EventArgs e)
+        {
+            zoom = zoom - 10;
+            canvas.Invalidate();
+        }
+
+        private void BtnPlus_Click(object sender, EventArgs e)
+        {
+            zoom = zoom + 10;
+            canvas.Invalidate();
+            brush = (Brush)Brushes.Beige;
+
+        }
+
     }
 }
